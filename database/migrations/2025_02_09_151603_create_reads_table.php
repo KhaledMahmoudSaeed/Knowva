a8968797d9fea2cb82a4c0ce3d321b7934bb7c7b
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-02-10 01:06:10 +0200

    feat: add speaking,reading,writing,listen tabels,models,factories,seeders

---
+++ database/migrations/2025_02_09_151603_create_reads_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Lang;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reads', function (Blueprint $table) {
+            $table->id();
+            $table->string("content");
+            $table->string("level");
+            $table->foreignIdFor(Lang::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reads');
+    }
+};
