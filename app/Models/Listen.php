d0920aa177b1de7c959159e445874c6a922fe7ca
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-02-10 11:49:53 +0200

    fix: make all attribute fillable

--- app/Models/Listen.php
+++ app/Models/Listen.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Listen extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
 }
--- app/Models/Write.php
+++ app/Models/Write.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Write extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
 }
