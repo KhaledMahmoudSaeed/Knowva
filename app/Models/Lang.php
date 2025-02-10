de2ad173ba8854ffb23e93eb30b9b42b2aaac475
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-02-10 17:00:02 +0200

    feat: add relations methods

--- app/Models/Lang.php
+++ app/Models/Lang.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Lang extends Model
 {
     use HasFactory;
     protected $guarded = [];
-
+    public function reading(): HasMany
+    {
+        return $this->hasMany(Read::class);
+    }
+    public function writing(): HasMany
+    {
+        return $this->hasMany(Write::class);
+    }
+    public function listing(): HasMany
+    {
+        return $this->hasMany(Listen::class);
+    }
+    public function speaking(): HasMany
+    {
+        return $this->hasMany(Speak::class);
+    }
 }
--- app/Models/Write.php
+++ app/Models/Write.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Write extends Model
 {
     use HasFactory;
     protected $guarded = [];
-
+    public function lang(): BelongsTo
+    {
+        return $this->belongsTo(Lang::class);
+    }
 }
