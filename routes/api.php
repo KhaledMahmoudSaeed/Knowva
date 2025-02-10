26bf4bb46eaf204ccee1a1a8ae4680cbf7aafbcc
KhaledMahmoudSaeed <khaild22k12m71f@gmail.com>
2025-02-10 17:01:29 +0200

    feat: add routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\LangController;
+use App\Http\Controllers\ListenController;
+use App\Http\Controllers\ReadController;
+use App\Http\Controllers\SpeakController;
+use App\Http\Controllers\WriteController;
 use App\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,7 +31,26 @@ Route::middleware(HandleCors::class)->group(function () {
     Route::post('/login', [LoginController::class, 'login'])->name('login');
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     Route::post('/register', [RegisterController::class, 'register'])->name("register");
+    // lang api
     Route::get("/lang", [LangController::class, 'index'])->name("lang.index");
     Route::post("/lang", [LangController::class, 'store'])->name("lang.store");
     Route::delete("/lang/{id}", [LangController::class, 'destroy'])->name("lang.destroy");
+    // reading api
+    Route::get("/read", [ReadController::class, 'index'])->name("read.index");
+    Route::post("/read", [ReadController::class, 'store'])->name("read.store");
+    Route::delete("/read/{id}", [ReadController::class, 'destroy'])->name("read.destroy");
+    //writing api
+    Route::get("/write", [WriteController::class, 'index'])->name("write.index");
+    Route::post("/write", [WriteController::class, 'store'])->name("write.store");
+    Route::delete("/write/{id}", [WriteController::class, 'destroy'])->name("write.destroy");
+    //speaking api
+    Route::get("/speak", [SpeakController::class, 'index'])->name("speak.index");
+    Route::post("/speak", [SpeakController::class, 'store'])->name("speak.store");
+    Route::delete("/speak/{id}", [SpeakController::class, 'destroy'])->name("speak.destroy");
+    //listening api
+    Route::get("/listen", [ListenController::class, 'index'])->name("listen.index");
+    Route::post("/listen", [ListenController::class, 'store'])->name("listen.store");
+    Route::delete("/listen/{id}", [ListenController::class, 'destroy'])->name("listen.destroy");
+
+
 });
